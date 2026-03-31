105e0177996ba8be3eb919a752e80f3601856e59
abogazee515-droid <abogazee515@gmail.com>
2026-03-31 15:06:11 +0700

    Add input validation for array size

--- insertion.cpp
+++ insertion.cpp
@@ -6,3 +6,17 @@ int arr[20];
 
 // Variable to store number of elements
 int n;
+
+void input() {
+    while (true) {
+        cout << "Enter the Number of Data in the Array: ";
+        cin >> n;
+
+        // Validate input (max 20 elements)
+        if (n <= 20) {
+            break;
+        }
+        else {
+            cout << "\nArray cannot be more than 20\n";
+        }
+    }
