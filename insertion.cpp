15f27427937617897a2b2fb3d9f0e3498e080f30
abogazee515-droid <abogazee515@gmail.com>
2026-03-31 15:10:56 +0700

    Display final sorted array

--- insertion.cpp
+++ insertion.cpp
@@ -55,3 +55,15 @@ void insertionsort() {
         }
     }
 }
+
+void display() {
+    cout << endl;
+    cout << "\n===========================";
+    cout << "\nSorted Array Elements" << endl;
+    cout << "\n===========================";
+
+    for (int j = 0; j < n; j++) {
+        cout << arr[j] << endl;
+    }
+    cout << endl;
+}
